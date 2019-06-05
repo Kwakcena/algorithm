47c7ee32cc117ba78db8557cd993128b05b13f20
KwakCena <rhkrgudwh@gmail.com>
2019-06-05 13:03:19 +0900

    1004번 어린 왕자 문제 소스
    "출발점 - 행성, 도착점 - 행성" 방식으로 진입/이탈 변수를 카운트 하지 않고, 하나의 result 변수로
    카운트 했다. 예외로 한 행성에 같이 있을때, 밖에 있을때가 있었음.

--- baekjoon/1004_어린_왕자/1004.cpp
+++ baekjoon/1004_어린_왕자/1004.cpp
@@ -5,10 +5,6 @@
 
 using namespace std;
 
-
-//진입, 이탈 변수
-int enter, derail;
-
 int main()
 {
 	int T, n;
@@ -16,6 +12,8 @@ int main()
 
 	scanf("%d", &T);		//테스트 케이스 입력
 	while(T--) {
+		//진입, 이탈 변수
+		int result = 0;
 		//출발점과 도착점 입력
 		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 		//행성계의 개수 입력
@@ -30,18 +28,13 @@ int main()
 			double start_d = sqrt(pow(ABS_DIFF(x1, c_x), 2) + pow(ABS_DIFF(y1, c_y), 2)); 
 			double end_d = sqrt(pow(ABS_DIFF(x2, c_x), 2) + pow(ABS_DIFF(y2, c_y), 2)); 
 
-			if(start_d < r || (x1==c_x && y1==c_y)) derail++;
-			if(end_d < r || (x2==c_x && y2==c_y)) enter++;
-			if(start_d + end_d < 2 * r) {
-				derail--;
-				enter--;
+			if(start_d < r && end_d < r) continue;
+			else if(start_d < r || end_d < r) {
+				result += 1;	
 			}
 		}
 
-		printf("%d\n", enter + derail);
-
-		enter = 0;
-		derail = 0;
+		printf("%d\n" ,result);
 	}
 
 	return 0;
