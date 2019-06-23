c7cace263d214c55e80d6839d70d28596093f361
KwakCena <rhkrgudwh@gmail.com>
2019-06-23 12:38:22 +0900

    D5 문제 추가
    7812번 정렬을 병합정렬로 바꿈. 시간초과는 나지 않는데 테스트케이스 1개만 맞았음

--- SW-Expert-Academy/D5/7812.cpp
+++ SW-Expert-Academy/D5/7812.cpp
@@ -2,16 +2,54 @@
 
 using namespace std;
 
-void sort(int arr[], int idx) 
+int N;
+
+void merging(int arr[], int s1, int e1, int s2, int e2)
 {
-	for(int i=0; i<idx; i++) {
-		for(int j=i+1; j<idx; j++) {
-			if(arr[i] > arr[j]) {
-				int temp = arr[j];
-				arr[j] = arr[i];
-				arr[i] = temp;
-			}
+	int p, q;
+	int temp[N];
+	int temp_idx = 0;
+
+	p = s1;
+	q = s2;
+
+	while(p <= e1 && q <= e2) {
+		if(arr[p] < arr[q]) {
+			temp[temp_idx++] = arr[p];
+			p++;
 		}
+		else {
+			temp[temp_idx++] = arr[q];
+			q++;
+		}
+	}
+
+	if(p > e1) {
+		for(int i=q; i<=e2; i++) {
+			temp[temp_idx++] = arr[i];
+		}
+	}
+	else if(q > e2){
+		for(int i=p; i<=e1; i++) {
+			temp[temp_idx++] = arr[i];
+		}
+	}
+
+	for(int i=s1; i<=e2; i++) {
+		arr[i] = temp[i-s1];
+	}
+}
+
+void mergeSort(int arr[], int start, int end)
+{
+	if(start >= end) {
+		return;
+	}
+	else {
+		int mid = (start + end) / 2;
+		mergeSort(arr, start, mid);
+		mergeSort(arr, mid+1, end);
+		merging(arr, start, mid, mid+1, end);
 	}
 }
 
@@ -23,7 +61,7 @@ int main()
 
 	while(T--)
 	{
-		int N, M;
+		int M;
 		scanf("%d %d", &N, &M);
 
 		int arr[N];
@@ -31,8 +69,7 @@ int main()
 			scanf("%d", &arr[i]);
 		}
 
-		sort(arr, N);
-
+		mergeSort(arr, 0, N-1);
 		for(int i=0; i<N; i++) {
 			M -= arr[i];
 			cnt++;
