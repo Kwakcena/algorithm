eb9acaebc25c747001eff0ce88de6ff65a8ed1a3
KwakCena <rhkrgudwh@gmail.com>
2019-07-05 19:55:42 +0900

    퀵 정렬 추가 (진행 중)
    퀵 정렬을 추가해서 정렬의 속도를 올리고자 하는데 정렬이 잘 안되고
    있음... 뭐가문제일까

--- baekjoon/1764_듣보잡/1764.cpp
+++ baekjoon/1764_듣보잡/1764.cpp
@@ -2,19 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-void Sort(char str[][21], int a_len)
+int N, M, idx = 0;
+
+int getLeft(char arr[][21], char left[][21], int start, int end, const char* pivot)
 {
-	for(int i=0; i<a_len; i++) {
-		int idx = i;
-		for(int j=i+1; j<a_len; j++) {
-			if(strcmp(str[idx], str[j]) > 0) {
-				char temp[21];
-				strcpy(temp, str[idx]);
-				strcpy(str[idx], str[j]);
-				strcpy(str[j], temp);
-			}
+	int idx = 0;
+	for(int i=start; i<=end; i++) {
+		if(strcmp(arr[i], pivot) < 0) {
+			strcpy(left[idx++], arr[i]);
 		}
 	}
+
+	return idx;
+}
+
+int getRight(char arr[][21], char right[][21], int start, int end, const char* pivot)
+{
+	int idx = 0;
+	for(int i=start; i<=end; i++) {
+		if(strcmp(arr[i], pivot) >= 0) {
+			strcpy(right[idx++], arr[i]);
+		}
+	}
+
+	return idx;
+}
+
+void quickSort(char arr[][21], int start, int end)
+{
+	if(start >= end) return;
+
+	const char *pivot = arr[start];
+	char left[N+M][21], right[N+M][21];
+
+	int left_cnt = getLeft(arr, left, start+1, end, pivot);
+	int right_cnt = getRight(arr, right, start+1, end, pivot);
+	
+	printf("\n==================\n");
+	printf("pivot : %s\n", pivot);
+	printf("***getLeft\n");
+	for(int i=0; i<left_cnt; i++) printf("%s\n", left[i]);
+	printf("***getRight\n");
+	for(int i=0; i<right_cnt; i++) printf("%s\n", right[i]);
+	printf("==================\n");
+
+	for(int i=0; i<left_cnt; i++) {
+		strcpy(arr[start+i], left[i]);
+	}
+	strcpy(arr[start+left_cnt], pivot);
+	for(int i=0; i<right_cnt; i++) {
+		strcpy(arr[start+left_cnt+1+i], right[i]);
+	}
+
+	printf("\n==================\n");
+	printf("***arr\n");
+	for(int i=0; i<end; i++) {
+		printf("%s\n", arr[i]);
+	}
+	printf("\n==================\n");
+
+
+	quickSort(arr, start, start+left_cnt-1);
+	quickSort(arr, start+left_cnt+1, end);
 }
 
 //이진탐색 비 재귀
@@ -49,7 +98,6 @@ int binarySearch(char arr[][21], int myStart, int myEnd, char value[])
 int main()
 {
 
-	int N, M, idx = 0;
 	scanf("%d %d", &N, &M);
 
 	//듣도 보도 못한
@@ -66,17 +114,20 @@ int main()
 	for(int i=0; i<M; i++) {
 		scanf("%s", see_list[i]);
 	}
-	Sort(see_list, M);
+	quickSort(see_list, 0, M-1);
 
+	printf("----- SORT\n");
+	for(int i=0; i<M; i++) {
+		printf("%s\n", see_list[i]);
+	}
 	for(int i=0; i<N; i++) {
 		if(binarySearch(see_list, 0, M-1, d_list[i]) != -1) {
 			strcpy(result[idx++], d_list[i]);
 		}
 	}
 
-	//printf("%d\n", binarySearch(see_list, 0, M-1, d_list[0]));
-
-	Sort(result, idx);
+	quickSort(result, 0, idx-1);
+	printf("---\n");
 	for(int i=0; i<idx; i++) {
 		printf("%s\n", result[i]);
 	}
