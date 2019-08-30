a001c959ca3340acc0e12fb0b2eb07e64791b556
KwakCena <rhkrgudwh@gmail.com>
2019-08-31 01:42:45 +0900

    14502번 연구소 문제 풀이
    이 문제를 풀면서 알고리즘의 설계는 역시 "완전 탐색" 부터 시작해야
    된다는걸 다시금 느꼈다. 처음부터 완전탐색은 시간초과가 날 것으로
    단정짓고 완전탐색을 고려 대상에 포함시키지 않는 태도로부터 정답과
    멀어지는 느낌을 받았다.
    
    "벽을 어떻게 효율적으로 처음부터 세울까?" 라는 생각에 바이러스를
    퍼트리면서 벽으로 한번 막아볼까 하는 생각도 했지만 그냥 벽을 세군대
    새우고 바이러스를 퍼트려본다음 안전구역의 갯수를 새면 되는 문제였다.
    
    1. 재귀를 이용해 안전구역 3군대에 벽을 세운다.
    2. 벽이 3개가 세워지면 BFS를 돌려 바이러스를 퍼트린다.
    3. 바이러스가 퍼진 뒤 0의 개수를 샌다.
    
    위 과정을 반복한 다음 최대 안전구역이 나오는 경우를 출력하면 답이다.

--- baekjoon/BFS_Problem/연구소-14502-c++.cpp
+++ baekjoon/BFS_Problem/연구소-14502-c++.cpp
@@ -1,49 +1,56 @@
+#include <unistd.h>
 #include <cstdio>
 #include <bits/stdc++.h>
 
 #define MX_LEN 10
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 using namespace std;
 
-int N, M;
-int Table[MX_LEN][MX_LEN], visited[MX_LEN][MX_LEN];
+int N, M, result;
+int Table[MX_LEN][MX_LEN];
 int dy[] = {-1, +1, 0, 0}, dx[] = {0, 0, -1, +1};
 
 vector<pair<int, int> > virus_list, safe_zone;
 
-void testPrint()
+int BFS(int map[MX_LEN][MX_LEN])
 {
-	printf("====================\n");
-	for(int i=0; i<N; i++) {
-		for(int j=0; j<M; j++) {
-			printf("%d ", Table[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-int BFS()
-{
+	int cnt = 0;
 	queue<pair<int, int> > pos_q;
 	for(int i=0; i<virus_list.size(); i++) pos_q.push(virus_list[i]);
 
 	while(!pos_q.empty()) {
 		pair<int, int> curt = pos_q.front();
+		pos_q.pop();
 
 		for(int i=0; i<4; i++) {
 			int r = curt.first + dy[i];
 			int c = curt.second + dx[i];
 
 			if(r < 0 || r >= N || c < 0 || c >= M) continue;
+			if(map[r][c] == 0) {
+				map[r][c] = 2;
+				pos_q.push(make_pair(r, c));
+			}
 		}
 	}
+
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<M; j++) {
+			if(map[i][j] == 0) cnt++;
+		}
+	}
+
+	return cnt;
 }
 
 
 void makeWall(int idx, int cnt)
 {
 	if(cnt == 3) {
-		testPrint();
+		int map[MX_LEN][MX_LEN];
+		memcpy(map, Table, sizeof(Table));
+		result = MAX(result, BFS(map));
 		return;
 	}
 	for(int i=idx; i<safe_zone.size(); i++) {
@@ -65,6 +72,7 @@ int main()
 	}
 
 	makeWall(0, 0);
+	printf("%d", result);
 	
 	return 0;
 }
