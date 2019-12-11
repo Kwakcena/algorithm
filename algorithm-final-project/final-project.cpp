6005f68544fd2346c115ab883017b8561825b259
KwakCena <rhkrgudwh@gmail.com>
2019-12-12 01:45:02 +0900

    테스트 좌표와 전파 도달 거리 입력받기
    테스트 좌표를 설정하고 정점은 10개로 정해놨다. 전파 도달거리는 실수로 입력받도록 한다.

--- algorithm-final-project/final-project.cpp
+++ algorithm-final-project/final-project.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
+const int N = 10;
+
+typedef struct pos {
+    int x, y, z;
+} POS;
+
+POS test_pos[N] = {
+    {1, 1, 1},
+    {4, 9, 3},
+    {14, 2, 3},
+    {3, 8, 0},
+    {0, 23, 3},
+    {6, 2, 9},
+    {1, 3, 3},
+    {21, 7, 3},
+    {1, 4, 0},
+    {9, 2, 3}
+};
+
 int main()
 {
+    double r;
+    cout << "전파 도달거리 입력 : ";
+    cin >> r;
+
+    for(int i=0; i<N; i++) {
+        cout << i << "번 : " << test_pos[i].x << " " << test_pos[i].y << " " << test_pos[i].z << endl;
+    }
+
+
     return 0;
 }
