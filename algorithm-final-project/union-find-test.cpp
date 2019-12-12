bd95bae6e7091aa63d86e13dc9dc3723bca91048
KwakCena <rhkrgudwh@gmail.com>
2019-12-12 17:48:46 +0900

    기능 추가
    * 함수명을 카멜 표기법으로 수정
    * 테스트 케이스 몇개 더 추가
    * 구조체 앞에 typedef 키워드 추가
    * 부모 노드가 같은지 알려주는 findParent 구조체 멤버 함수 추가
    * 실수값 R보다 작으면 정점을 연결해주는 vertex connection 함수 추가

--- algorithm-final-project/union-find-test.cpp
+++ algorithm-final-project/union-find-test.cpp
@@ -2,13 +2,15 @@
 #include <cmath>
 #include <vector>
 
+#define N 10
+
 using namespace std;
 
-struct pos {
+typedef struct pos {
     int x, y, z;
-};
+} POS;
 
-struct DisjointSet {
+typedef struct DisjointSet {
     vector<int> parent, rank;
 
     DisjointSet(int n): parent(n), rank(n,1) {
@@ -21,42 +23,72 @@ struct DisjointSet {
         return parent[u] = find(parent[u]);
     }
 
+    int findParent(int u, int v) {
+        u = find(u); v = find(v);
+        return u == v ? 1 : 0;
+    }
+
     void merge(int u, int v) {
         u = find(u); v = find(v);
 
         if(u == v) return;
         if(rank[u] > rank[v]) swap(u, v);
-        parent[u] = v;
+        parent[v] = u;
 
         if(rank[u] == rank[v]) ++rank[v];
     }
-};
+}DS;
 
-double distance_between_two_points(struct pos A, struct pos B)
+double distanceBetweenTwoPoints(POS A, POS B)
 {
     cout << "A의 좌표 : (x : " << A.x << ", y : " << A.y << ", z : " << A.z << ")" << endl;
     cout << "B의 좌표 : (x : " << B.x << ", y : " << B.y << ", z : " << B.z << ")" << endl;
 
     double base = sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2)); 
-    cout << "밑변 : " << base << endl;
-
     double height = B.z - A.z;
-    cout << "높이 : " << height << endl;
 
+    cout << "거리 : " << sqrt(pow(base, 2) + pow(height, 2)) << endl;
     return sqrt(pow(base, 2) + pow(height, 2));
+}
+
+void testPrint(DS ds)
+{
+    for(int i=0; i<N; i++) {
+        cout << "node : " << i << " / parent : " << ds.find(i) << endl;
+    }
+}
+
+void vertexConnection(POS posData[], DS *ds, double R)
+{
+    for(int i=0; i<N; i++) {
+        for(int j=i+1; j<N; j++) {
+            cout << "( " << i << " " << j << " )" << endl;
+           if(R >= distanceBetweenTwoPoints(posData[i], posData[j])) {
+               ds->merge(i, j);
+           }
+        }
+    }
 }
 
 int main()
 {
-    struct DisjointSet ds(10);
-    struct pos test_pos[3] = {
-        {1, 1, 1},
-        {2, 2, 2},
-        {3, 3, 3}
+    DS ds(N + 1);
+    POS test_pos[N] = {
+        {1, 0, 11},
+        {12, 0, 2},
+        {3, 2, 10},
+        {4, 3, 8},
+        {0, 1, 6},
+        {7, 2, 1},
+        {13, 22, 0},
+        {9, 3, 1},
+        {1, 3, 3},
+        {0, 0, 0},
     };
+   
 
-    double test = distance_between_two_points(test_pos[0], test_pos[2]);
-    cout << test;
+    vertexConnection(test_pos, &ds, 15.0);
+    testPrint(ds);
 
     return 0;
 }
